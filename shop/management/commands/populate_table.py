8160732f7da3f1372bde5c54c3e02eca5c1505d8
HANGYU LIU <h.liu1.20@abdn.ac.uk>
2022-05-04 08:58:02 +0000

    populate bales

--- shop/management/commands/populate_table.py
+++ shop/management/commands/populate_table.py
@@ -1,3 +1,6 @@
 import random,decimal
 from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from faker import Faker
+from django.core.management.base import BaseCommand,CommandError
